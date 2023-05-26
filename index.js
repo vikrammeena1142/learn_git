2343566c1b2371e6403221dcb040be78beae3ee0
Vikram meena <Vikrammeena1142@gmail.com>
2023-05-26 16:55:31 +0530

    modified in index

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ for (let index = 0; index < 10; index++) {
    console.log(index)
 }
 for (let index = 0; index < 10; index++) {
-   console.log("I love front-end devlopement")
+   console.log("I love my mom "+" "+index)
 }
